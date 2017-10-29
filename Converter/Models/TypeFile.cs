e5d6d058b5f76268080251ed8e8113c7a563d0b1
mjarek <mjarek1313@gmail.com>
2017-10-29 15:51:40 +0100

    Start page with controls plus Model for data
    I did start page but without formating etc.
    
    I added model for data which will put into controls.
    
    Zrobiłem stronę startową. Na razie

---
+++ Converter/Models/TypeFile.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Converter.Models
+{
+    public class TypeFile
+    {
+
+        
+        public enum TypeFiles
+        {
+            Csv,
+            Xml
+        }
+
+
+    }
+}
