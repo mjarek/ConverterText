c80c47ef0e1197ea6fdaf9ddf799d89586946b07
mjarek <mjarek1313@gmail.com>
2017-11-01 14:18:59 +0100

    file Utf8StringWriter

---
+++ Converter/Service/Utf8StringWriter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace Converter.Service
+{
+    public class Utf8StringWriter : StringWriter
+    {
+
+        public override Encoding Encoding
+        {
+            get { return new UTF8Encoding(false); }
+        }
+    }
+}
